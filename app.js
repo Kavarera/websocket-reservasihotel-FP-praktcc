0399f11dc0c9b95e21369976ec539c991b9d7c93
Kavarera <rafli.030715@gmail.com>
2024-06-05 01:08:44 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const cors = require('cors');
 
 const app = express();
+app.use(cors()); // Tambahkan middleware CORS
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
